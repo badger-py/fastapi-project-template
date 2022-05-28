4f729d3309412bfb491faf83ef549b4ce59e7def
Yan <64094918+badger-py@users.noreply.github.com>
2022-05-28 11:23:09 +0400

    One more grammar fix

--- backend/create_db.py
+++ backend/create_db.py
@@ -3,6 +3,6 @@ from db import Base, engine
 
 # create tables
 Base.metadata.create_all(bind=engine)
-print("db was created sucesfully")
+print("db was created successfully")
 
 # NOTE: you can use Faker libary to create fake data

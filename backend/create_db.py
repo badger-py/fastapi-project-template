ea88a9e6c152211edebd2cbc74fd6f7b966475b1
Yan <64094918+badger-py@users.noreply.github.com>
2022-05-28 11:22:27 +0400

    creates -> created

--- backend/create_db.py
+++ backend/create_db.py
@@ -3,6 +3,6 @@ from db import Base, engine
 
 # create tables
 Base.metadata.create_all(bind=engine)
-print("db was creates sucesfully")
+print("db was created sucesfully")
 
 # NOTE: you can use Faker libary to create fake data
